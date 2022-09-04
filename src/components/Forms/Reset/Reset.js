8d7df1056a60a6637f875b86d8fcf82e4280c5c9
Pericles001 <periclesadjovi@gmail.com>
2022-09-04 20:38:06 +0100

    Add route to success message page

--- src/components/Forms/Reset/Reset.js
+++ src/components/Forms/Reset/Reset.js
@@ -2,21 +2,22 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Form} from 'react-bootstrap';
 import {Link} from "react-router-dom";
-import {useLocation, useNavigate} from "react-router";
+import {Outlet, useLocation, useNavigate} from "react-router";
 
 export function Reset() {
     const navigate = useNavigate();
     const location = useLocation();
-    const toLog = () => {
-        navigate('/');
+    const toLog = (target) => {
+        navigate(target);
     }
 
     return (
         <div className="container-fluid">
+
             <div className="row">
                 <Form className="mt-5 mx-auto col-md-4 shadow" style={{backgroundColor: "#D9D9D9"}}>
                     <h4 className="text-center fw-semibold mt-3 mb-3" style={{cursor: "Pointer"}}
-                        onClick={toLog}>Quizreo</h4>
+                        onClick={toLog('/')}>Quizreo</h4>
                     <Form.Group controlId="formBasicEmail" className="mt-3">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="Enter email"/>
@@ -24,10 +25,13 @@ export function Reset() {
                             Password reset link will be sent to this email.
                         </Form.Text>
                     </Form.Group>
-
-                    <Button className="mt-3 mb-3" variant="secondary" size="lg">
+                    <Outlet/>
+                    <Button className="mt-3 mb-3" variant="secondary" size="lg" type="submit" onClick={() => {
+                        navigate('/reset_msg')
+                    }}>
                         Reset
                     </Button>
+
                     <Link className="d-block mt-3 mb-3 text-decoration-none" to="/signup">Signup here</Link>
                     <Link className="d-block mt-3 mb-3 text-decoration-none" to="/login">Login here</Link>
                 </Form>
