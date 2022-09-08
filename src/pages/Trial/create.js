e55133f6a96b436b447578bab5157cfa2caf72ee
Pericles001 <periclesadjovi@gmail.com>
2022-09-08 04:58:56 +0100

    Edit code to create trial form

---
+++ src/pages/Trial/create.js
@@ -0,0 +1,11 @@
+import {Footer, Trial} from "../../components";
+import React from "react";
+
+export function CreateTrialPage() {
+    return (
+        <div>
+            <Trial/>
+            <Footer/>
+        </div>
+    )
+}
