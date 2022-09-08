311523a5b86558892e9a9d4b3ad7ab2a86d85ce7
Pericles001 <periclesadjovi@gmail.com>
2022-09-08 04:58:37 +0100

    Edit code to create quiz form

---
+++ src/pages/Quiz/create.js
@@ -0,0 +1,12 @@
+import {Footer} from "../../components";
+import React from "react";
+import {Quiz} from "../../components/Forms";
+
+export function CreateQuizPage() {
+    return (
+        <div>
+            <Quiz/>
+            <Footer/>
+        </div>
+    )
+}
