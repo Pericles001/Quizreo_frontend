ffede7625a9150bfc0b367b8a5e3616b07eec2da
Pericles001 <periclesadjovi@gmail.com>
2022-09-08 20:35:36 +0100

    Edit to add share option

---
+++ src/pages/share/share.js
@@ -0,0 +1,10 @@
+import {ShareTable, Footer} from "../../components";
+
+export function SharePage(){
+    return (
+        <div className="container-fluid mt-5 p-3">
+            <ShareTable/>
+            <Footer/>
+        </div>
+    )
+}
