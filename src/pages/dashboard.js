1b44410693954e095aeae70f7a74b0ad4fff1397
Pericles001 <periclesadjovi@gmail.com>
2022-09-15 05:03:37 +0100

    Start setting up dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,37 @@
 import {Footer, SampleCard, Sidebar} from "../components";
 import {Breakpoint} from "react-socks";
+import {active} from "../components/Forms/Login/Login";
+import client from "../api/axios";
+import {useState} from "react";
 
 export function DashboardPage() {
+    const [users, setUsers] = useState([]);
+    const user = active;
+
+    const getAllUsers = async () => {
+        const response = await client.get(`/`, {
+            validateStatus: function (status) {
+                return status < 500; // Reject only if the status code is greater than or equal to 500
+            }
+        });
+        if (response.status === 302) {
+            setUsers(response.data)
+        }
+    }
+    getAllUsers().then(r => console.log("ok"));
+
+
+    console.log(user);
+    const displayUser = user.map((user) => {
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Welcome {user.username}</h5>
+                    <p className="card-text">You are logged in.</p>
+                </div>
+            </div>
+        )
+    })
     return (
         <div className="container-fluid">
             <Breakpoint small down>
@@ -24,7 +54,7 @@ export function DashboardPage() {
                         <Sidebar/>
                     </div>
                     <div className="col-9">
-                        <h2 className="mt-5 mb-5">Welcome <h6>Pericles Adjovi</h6></h2>
+                        {displayUser}
                         <SampleCard title="Trending" quote="Survey example title" author="Pericles Adjovi"/>
                         <SampleCard title="Top Rated" quote="Survey example title" author="Pericles Adjovi"/>
                         <SampleCard title="Top Users" quote="Survey example title" author="Pericles Adjovi"/>
